160fc3e2fd294de5dbec8061768c70b2aca52ac1
maurolcsilva <maurolcsilva@gmail.com>
2013-05-10 17:06:45 -0300

    Modificao da Mensagem de saida

--- UsandoColecoes/src/br/edu/ifma/minhacolecao/Principal.java
+++ UsandoColecoes/src/br/edu/ifma/minhacolecao/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Usando Coleções.");
+		System.out.println("Usando Coleções. E alterando a mensagem. Novamente");
 	}
 
 }
