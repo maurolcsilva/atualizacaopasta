cd1120d71df053813199e3e684b5ba5818915672
maurolcsilva <maurolcsilva@gmail.com>
2013-05-10 16:51:53 -0300

    Comit para inicializacao do Projeto.

---
+++ UsandoColecoes/src/br/edu/ifma/minhacolecao/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.ifma.minhacolecao;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Usando Coleções.");
+	}
+
+}
